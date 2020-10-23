af62b4702253d6677ef62ab6628157b1675b53a5
alexjbustillos <alexjbustillos@gmail.com>
2020-10-23 12:18:42 -0400

    [1and2] made changes to both

---
+++ clock.js
@@ -0,0 +1,4 @@
+const hour = document.querySelector('#hour');
+const face = document.querySelector('#face');
+const minute = document.querySelector('#minute');
+const second = document.querySelector('#second');
